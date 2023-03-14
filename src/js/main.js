eda34738ee0747d86c5b4d2b1f6835e3e65c5317
nikitawong19 <iamspecial19@gmail.com>
2023-03-14 23:20:44 +0800

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,13 @@
 import { InspirationalQuote } from "./quote";
 import { loadHeaderFooter } from "./utils";
 import { renderTemplateRecipes } from "./recipeSide";
+import { testimonial } from "./testimonial";
 
 loadHeaderFooter();
 
 renderTemplateRecipes();
+
+testimonial();
 
 const quote = new InspirationalQuote();
 quote.getQuote();
