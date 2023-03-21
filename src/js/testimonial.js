2a8c7e1db66017fad6c3f573179dfa0c6f93b400
nikitawong19 <iamspecial19@gmail.com>
2023-03-22 00:09:46 +0800

    Updated newsletter

--- src/js/testimonial.js
+++ src/js/testimonial.js
@@ -1,19 +1,19 @@
 export function testimonial() {
-    const test = document.createElement('div');
-    test.classList.add('testimonials');
+    const divOne = document.createElement("div");
+    divOne.classList.add("testimonials");
 
     const testimonials = [
         {
             name: "Diana Quispe",
-            message: "I love this product! It has made my life so much easier."
+            message: '"I love this product! It has made my life so much easier."'
         },
         {
             name: "Nikita Wong",
-            message: "This is the best product I have ever used. Highly recommended!"
+            message: '"This is the best product I have ever used. Highly recommended!"'
         },
         {
             name: "Josue Centurion",
-            message: "I was skeptical at first, but this product exceeded my expectation."
+            message: '"I was skeptical at first, but this product exceeded my expectation."'
         }
     ];
     
@@ -21,22 +21,32 @@ export function testimonial() {
     const jsonTestimonials = JSON.stringify(testimonials);
     console.log(jsonTestimonials);
     
-    // Parse or load the JSON data into JavaScrip object
+    // Parse or read the JSON data into JavaScrip object
     const objectTestimonial = JSON.parse(jsonTestimonials);
     console.log(objectTestimonial);
-    
+
+    // Create <h2> element
+    const title = document.createElement("h2");
+    title.classList.add("testimonialsTitle");
+    title.textContent = "Testimonials";
+    divOne.appendChild(title);
+
+    // Loop through each testimonial and create a new HTML element for it
     objectTestimonial.forEach(testimonial => {
-        
+        console.log(testimonial)
 
-        const divElement = document.createElement("div");
-        divElement.classList.add("customerTestimonial");
+        // Create a <div> element for the testimonial
+        const divTwo = document.createElement("div");
+        divTwo.classList.add("customerTestimonial");
 
-        divElement.innerHTML = `
+        // Add the testimonial name and message to the <div> element
+        divTwo.innerHTML = `
         <p>${testimonial.name}</p>
         <p>${testimonial.message}</p>
         `
         
-        test.appendChild(divElement);
+        divOne.appendChild(divTwo);
     });
-    return test;
+
+    return divOne;
 }
