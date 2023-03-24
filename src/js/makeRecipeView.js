085a0009abf60009133cc822a9a53fe0b2041009
Felix <fguzmanceri@gmail.com>
2023-03-24 15:23:18 -0400

    saving

--- src/js/makeRecipeView.js
+++ src/js/makeRecipeView.js
@@ -1,21 +1,21 @@
 export function makeRecipeView(recipe, storedRecipes) {
     const recipeContainer = document.createElement('div');
-    
+  
     const recipeNameDisplay = document.createElement('p');
     recipeNameDisplay.textContent = `Recipe Name: ${recipe.name}`;
     recipeNameDisplay.setAttribute('data-index', storedRecipes.length - 1);
     recipeContainer.appendChild(recipeNameDisplay);
-    
+  
     const ingredientsDisplay = document.createElement('p');
-    ingredientsDisplay.textContent = `Ingredients: ${recipe.ingredients}`;
+    ingredientsDisplay.textContent = `Ingredients: ${recipe.ingredients.join(', ')}`;
     ingredientsDisplay.setAttribute('data-index', storedRecipes.length - 1);
     recipeContainer.appendChild(ingredientsDisplay);
-    
+  
     const instructionsDisplay = document.createElement('p');
     instructionsDisplay.textContent = `Instructions: ${recipe.instructions}`;
     instructionsDisplay.setAttribute('data-index', storedRecipes.length - 1);
     recipeContainer.appendChild(instructionsDisplay);
-    
+  
     return recipeContainer;
   }
   
