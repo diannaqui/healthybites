b850482be1aaed4437ddbdc93d3968a9641638c3
Felix <fguzmanceri@gmail.com>
2023-03-29 12:48:33 -0400

    saving

--- src/js/spooncular.js
+++ src/js/spooncular.js
@@ -1,9 +1,9 @@
 const baseUrl = 'https://api.spoonacular.com';
-const searchEndpoint = '/food/ingredients/search';
+const ingredientEndpoint = '/food/ingredients/search';
 import {apiKeyNumber} from "./ExternalServices.mjs";
 
-export async function searchIngredients(query) {
-  const response = await fetch(`${baseUrl}${searchEndpoint}?apiKey=${apiKeyNumber}&query=${query}`);
+export async function getIngredients(query) {
+  const response = await fetch(`${baseUrl}${ingredientEndpoint}?apiKey=${apiKeyNumber}&query=${query}`);
   const data = await response.json();
-  return data.results;
+  return data;
 }
