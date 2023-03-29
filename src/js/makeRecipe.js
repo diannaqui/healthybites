344184a2415967307e5e666bba1fdd7cab2d230a
Felix <fguzmanceri@gmail.com>
2023-03-29 12:14:52 -0400

    saving

--- src/js/makeRecipe.js
+++ src/js/makeRecipe.js
@@ -1,113 +1,150 @@
-import { searchIngredients } from "./spooncular";
+import { searchIngredients, getIngredientNutrition } from "./spoonacular.js";
 
-export async function createRecipeForm() {
-  const recipeFormContainer = document.createElement('div');
-  recipeFormContainer.classList.add("makeYourRecipeContainer")
-  const title = document.createElement('h2');
-  title.textContent = 'Make your recipe';
-  const form = document.createElement('form');
+export function createRecipeForm() {
+  const recipeFormContainer = document.createElement("div");
+  recipeFormContainer.classList.add("makeYourRecipeContainer");
 
-  const nameLabel = document.createElement('label');
-  nameLabel.textContent = 'Recipe Name: ';
-  const nameInput = document.createElement('input');
-  nameInput.setAttribute('name', 'recipeName');
-  nameInput.setAttribute('required', true);
+  const title = document.createElement("h2");
+  title.textContent = "Make your recipe";
+
+  const form = document.createElement("form");
+
+  const nameLabel = document.createElement("label");
+  nameLabel.textContent = "Recipe Name: ";
+  const nameInput = document.createElement("input");
+  nameInput.setAttribute("name", "recipeName");
+  nameInput.setAttribute("required", true);
   form.appendChild(nameLabel);
   form.appendChild(nameInput);
 
-  const ingredientsDropdownLabel = document.createElement('label');
-  ingredientsDropdownLabel.textContent = 'Select an ingredient:';
-  const ingredientsDropdown = document.createElement('select');
-  ingredientsDropdown.setAttribute('name', 'ingredient');
-  ingredientsDropdown.setAttribute('required', true);
-
-  // use Spoonacular API to retrieve list of ingredients
-  const ingredients = await searchIngredients('');
-  
-  // create option elements for each ingredient and append to dropdown
-  ingredients.forEach(ingredient => {
-    const option = document.createElement('option');
-    option.setAttribute('value', ingredient.id);
-    option.textContent = ingredient.name;
-    ingredientsDropdown.appendChild(option);
+  const ingredientsLabel = document.createElement("label");
+  ingredientsLabel.textContent = "Ingredients: ";
+  const ingredientsContainer = document.createElement("div");
+  const ingredientsSelect = document.createElement("select");
+  ingredientsSelect.setAttribute("name", "recipeIngredients");
+  ingredientsSelect.setAttribute("required", true);
+  const defaultOption = document.createElement("option");
+  defaultOption.disabled = true;
+  defaultOption.selected = true;
+  defaultOption.textContent = "Select an ingredient...";
+  ingredientsSelect.appendChild(defaultOption);
+  const searchInput = document.createElement("input");
+  searchInput.setAttribute("type", "text");
+  searchInput.setAttribute("placeholder", "Search for an ingredient...");
+  searchInput.addEventListener("input", async () => {
+    const query = searchInput.value.trim();
+    if (query.length >= 3) {
+      const results = await searchIngredients(query);
+      const options = results.map((result) => {
+        const option = document.createElement("option");
+        option.value = result.id;
+        option.textContent = result.name;
+        return option;
+      });
+      ingredientsSelect.innerHTML = "";
+      ingredientsSelect.appendChild(defaultOption);
+      options.forEach((option) => {
+        ingredientsSelect.appendChild(option);
+      });
+    } else {
+      ingredientsSelect.innerHTML = "";
+      ingredientsSelect.appendChild(defaultOption);
+    }
   });
+  ingredientsContainer.appendChild(searchInput);
+  ingredientsContainer.appendChild(ingredientsSelect);
+  form.appendChild(ingredientsLabel);
+  form.appendChild(ingredientsContainer);
 
-  // add label and dropdown to form
-  form.appendChild(ingredientsDropdownLabel);
-  form.appendChild(ingredientsDropdown);
+  const nutrientsContainer = document.createElement("div");
+  nutrientsContainer.classList.add("nutrientsContainer");
+  form.appendChild(nutrientsContainer);
 
-  const instructionsLabel = document.createElement('label');
-  instructionsLabel.textContent = 'Instructions: ';
-  const instructionsTextarea = document.createElement('textarea');
-  instructionsTextarea.setAttribute('name', 'recipeInstructions');
-  instructionsTextarea.setAttribute('required', true);
+  const instructionsLabel = document.createElement("label");
+  instructionsLabel.textContent = "Instructions: ";
+  const instructionsTextarea = document.createElement("textarea");
+  instructionsTextarea.setAttribute("name", "recipeInstructions");
+  instructionsTextarea.setAttribute("required", true);
   form.appendChild(instructionsLabel);
   form.appendChild(instructionsTextarea);
 
-  const saveButton = document.createElement('button');
-  saveButton.textContent = 'Save';
+  const saveButton = document.createElement("button");
+  saveButton.textContent = "Save";
 
   // Define variables outside of the event listener
   let recipeNameDisplay, ingredientsDisplay, instructionsDisplay;
 
-  saveButton.addEventListener('click', (event) => {
+  // Function to get nutrient data for selected ingredient
+  async function getIngredient(ingredientId) {
+    const ingredient = await searchIngredients(ingredientId);
+    const nutrition = await getIngredientNutrition(ingredient.id);
+    return {
+      id: ingredient.id,
+      name: ingredient.name,
+      nutrition: nutrition,
+    };
+  }
+
+  saveButton.addEventListener("click", async (event) => {
     event.preventDefault();
-    const storedRecipes = JSON.parse(localStorage.getItem('recipes')) || [];
+    const storedRecipes = JSON.parse(localStorage.getItem("recipes")) || [];
     const recipeName = nameInput.value;
-    const recipeIngredients = ingredientsTextarea.value;
+    const ingredientId = ingredientsSelect.value;
     const recipeInstructions = instructionsTextarea.value;
+
+    const selectedIngredient = await getIngredient(ingredientId);
+
     const newRecipe = {
       name: recipeName,
-      ingredients: recipeIngredients,
+      ingredient: selectedIngredient,
       instructions: recipeInstructions,
     };
+
     storedRecipes.push(newRecipe);
-    localStorage.setItem('recipes', JSON.stringify(storedRecipes));
-    nameInput.value = '';
-    ingredientsTextarea.value = '';
-    instructionsTextarea.value = '';
-  
-    recipeNameDisplay = document.createElement('p');
-    recipeNameDisplay.textContent = `Recipe Name: ${newRecipe.name}`;
-    form.appendChild(recipeNameDisplay);
-  
-    ingredientsDisplay = document.createElement('p');
-    ingredientsDisplay.textContent = `Ingredients: ${newRecipe.ingredients}`;
-    form.appendChild(ingredientsDisplay);
-  
-    instructionsDisplay = document.createElement('p');
-    instructionsDisplay.textContent = `Instructions: ${newRecipe.instructions}`;
+    localStorage.setItem("recipes", JSON.stringify(storedRecipes));
     
-    // Add data-index attribute to each display element
-    const index = storedRecipes.length - 1;
-    recipeNameDisplay.setAttribute('data-index', storedRecipes.length - 1);
-    ingredientsDisplay.setAttribute('data-index', storedRecipes.length - 1);
-    instructionsDisplay.setAttribute('data-index', storedRecipes.length - 1);
-
-    form.appendChild(instructionsDisplay);
+    // Display saved recipe on the page
+    recipeNameDisplay.textContent = recipeName;
+    ingredientsDisplay.innerHTML = `<p><strong>${selectedIngredient.name}</strong></p><ul><li>Calories: ${selectedIngredient.nutrition.calories}</li><li>Protein: ${selectedIngredient.nutrition.protein}</li><li>Fat: ${selectedIngredient.nutrition.fat}</li><li>Carbs: ${selectedIngredient.nutrition.carbs}</li></ul>`;
+    instructionsDisplay.textContent = recipeInstructions;
+    
+    // Reset form
+    nameInput.value = "";
+    searchInput.value = "";
+    ingredientsSelect.innerHTML = "";
+    ingredientsSelect.appendChild(defaultOption);
+    instructionsTextarea.value = "";
   });
-  
-  const deleteAllButton = document.createElement('button');
-  deleteAllButton.textContent = 'Delete All';
-  deleteAllButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    localStorage.removeItem('recipes');
-    form.querySelectorAll('[data-index]').forEach(element => {
-      element.remove();
-    });
-  });
-   //define a container for the buttons
-   const buttonsContainer = document.createElement('div');
-   buttonsContainer.classList.add('buttonsContainer');
-   //add buttons to container
-   buttonsContainer.appendChild(saveButton);
-   buttonsContainer.appendChild(deleteAllButton);
-   //add container to form
-   form.appendChild(buttonsContainer);
-   
-   recipeFormContainer.appendChild(title);
-   recipeFormContainer.appendChild(form);
 
+  form.appendChild(saveButton);
   
-  return recipeFormContainer;
-}
+  recipeFormContainer.appendChild(title);
+  recipeFormContainer.appendChild(form);
+  
+  // Display saved recipe
+  const recipeDisplayContainer = document.createElement("div");
+  recipeDisplayContainer.classList.add("recipeDisplayContainer");
+  
+  const savedRecipes = JSON.parse(localStorage.getItem("recipes")) || [];
+  savedRecipes.forEach((recipe) => {
+  const recipeName = document.createElement("h3");
+  recipeName.textContent = recipe.name;
+  recipeDisplayContainer.appendChild(recipeName);
+  const ingredients = document.createElement("div");
+  ingredients.innerHTML = `<p><strong>${recipe.ingredient.name}</strong></p><ul><li>Calories: ${recipe.ingredient.nutrition.calories}</li><li>Protein: ${recipe.ingredient.nutrition.protein}</li><li>Fat: ${recipe.ingredient.nutrition.fat}</li><li>Carbs: ${recipe.ingredient.nutrition.carbs}</li></ul>`;
+  recipeDisplayContainer.appendChild(ingredients);
+  
+  const instructions = document.createElement("p");
+  instructions.textContent = recipe.instructions;
+  recipeDisplayContainer.appendChild(instructions);
+  
+  // Store variables for later use in event listener
+  recipeNameDisplay = recipeName;
+  ingredientsDisplay = ingredients;
+  instructionsDisplay = instructions;
+});
+
+recipeFormContainer.appendChild(recipeDisplayContainer);
+
+return recipeFormContainer;
+}  
