26d65cd346f465158550a5bc2ac8f5d193699b5e
Felix <fguzmanceri@gmail.com>
2023-03-29 11:11:03 -0400

    saving

--- src/js/spooncular.js
+++ src/js/spooncular.js
@@ -2,7 +2,7 @@ const apiKey = '96d8289ad2224dbdb0a5dbb77d49ea0f';
 const baseUrl = 'https://api.spoonacular.com';
 const searchEndpoint = '/food/ingredients/search';
 
-async function searchIngredients(query) {
+export async function searchIngredients(query) {
   const response = await fetch(`${baseUrl}${searchEndpoint}?apiKey=${apiKey}&query=${query}`);
   const data = await response.json();
   return data.results;
