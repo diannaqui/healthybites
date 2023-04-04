06831c6867ba6ac1c7a1026653778fb1fd7d20fd
nikitawong19 <iamspecial19@gmail.com>
2023-04-05 01:19:09 +0800

    Style the search result page

--- src/js/searchResult.js
+++ src/js/searchResult.js
@@ -2,11 +2,14 @@ import ExternalServices from "./ExternalServices.mjs"
 
 export async function searchResult(searchTerm) {
   const searchResultContainer = document.createElement("div");
+  searchResultContainer.classList.add("searchResultContainer");
 
   const h2 = document.createElement("h2");
-  h2.textContent = "Search Result";
+  h2.textContent = "Here's your search result";
+  h2.classList.add("searchResultTitle")
 
   const ul = document.createElement("ul");
+  ul.classList.add("ulForSearchResult")
 
   const food = new ExternalServices();
   try {
@@ -27,6 +30,7 @@ export async function searchResult(searchTerm) {
         const a = document.createElement('a'); // Create a link to click
         a.href = `#/src/js/recipePageDetailsEntrance?idSelected=${results.id}`;
         a.textContent = "See Recipe";
+        a.classList.add("linkToRecipe")
         li.appendChild(a)
 
         ul.appendChild(li);
